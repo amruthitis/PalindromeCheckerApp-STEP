cdfc5d25273c53ffd9262bf424e88e5b6f7779c0
amruthitis <amruthteki@gmail.com>
2026-02-23 16:25:38 +0530

    Finished UC2

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,26 @@
 import java.util.Scanner;
 
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
+    public static boolean checkPalindrome(String n){
+        n = n.toLowerCase();
+        int i=0, j= n.length() - 1;
+        while (i<j){
+            if (n.charAt(i) != n.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
         System.out.println("------ Welcome to Palindrome Checker App --------");
+        System.out.println("Version : 1.0");
+        System.out.println("System Initialised Successfully");
+
+        Scanner scanner = new Scanner(System.in);
+        String name =  scanner.nextLine();
+        System.out.println(checkPalindrome(name));
     }
 }
+
