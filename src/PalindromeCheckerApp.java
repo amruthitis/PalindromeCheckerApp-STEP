34195cfca4dd01fe5b167eb079c17693afa03183
amruthitis <amruthteki@gmail.com>
2026-03-02 14:49:17 +0530

    UC3

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,7 @@
-
 public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        String word = "madam";
+
+    // UC2: Brute Force Method
+    public static void checkPalindromeBruteForce(String word) {
         boolean isPalindrome = true;
         int length = word.length();
 
@@ -13,9 +13,32 @@ public class PalindromeCheckerApp {
         }
 
         if (isPalindrome) {
-            System.out.println(word + " is a Palindrome");
+            System.out.println(word + " is a Palindrome (Brute Force)");
         } else {
-            System.out.println(word + " is not a Palindrome");
+            System.out.println(word + " is not a Palindrome (Brute Force)");
         }
+    }
+
+    // UC3: String Reverse Method
+    public static void checkPalindromeByReverse(String word) {
+        String reversed = "";
+        int length = word.length();
+
+        for (int i = length - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);  // String Concatenation
+        }
+
+        if (word.equals(reversed)) {  // equals() compares content not reference
+            System.out.println(word + " is a Palindrome (String Reverse)");
+        } else {
+            System.out.println(word + " is not a Palindrome (String Reverse)");
+        }
+    }
+
+    public static void main(String[] args) {
+        String word = "madam";
+
+        checkPalindromeBruteForce(word);
+        checkPalindromeByReverse(word);
     }
 }
